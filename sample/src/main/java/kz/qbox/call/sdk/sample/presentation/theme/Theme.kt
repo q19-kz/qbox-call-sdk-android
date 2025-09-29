fdd61df3ab393738dc70652124b50743f6991e9e
rsaliyevzhomart <rsaliyevzhomart@gmail.com>
2025-09-29 12:41:50 +0500

    Release 1.1.0 (11)

--- sample/src/main/java/kz/qbox/call/sdk/sample/presentation/theme/Theme.kt
+++ sample/src/main/java/kz/qbox/call/sdk/sample/presentation/theme/Theme.kt
@@ -57,6 +57,7 @@ fun QBoxCallSDKTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
+            @Suppress("DEPRECATION")
             window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
